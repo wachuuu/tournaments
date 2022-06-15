2731105288116ae7981fd797632de58a9bbdf566
wachuuu <gab.wachowski15@gmail.com>
2022-06-15 09:57:54 +0200

    firebase deploy setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyB3U1qMufWjjpPGkPPGPf45OWUErC4KlOI",
+    authDomain: "tournaments-eca72.firebaseapp.com",
+    projectId: "tournaments-eca72",
+    storageBucket: "tournaments-eca72.appspot.com",
+    messagingSenderId: "886331757664",
+    appId: "1:886331757664:web:379e6653055a7bd265df05"
+  }
 };
