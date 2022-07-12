7301d8e0d1e8fbab1d4d47afa9c0fee71613de1d
wachuuu <gab.wachowski15@gmail.com>
2022-07-12 10:48:34 +0200

    hide firebase credentials

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyB3U1qMufWjjpPGkPPGPf45OWUErC4KlOI",
-    authDomain: "tournaments-eca72.firebaseapp.com",
-    projectId: "tournaments-eca72",
-    storageBucket: "tournaments-eca72.appspot.com",
-    messagingSenderId: "886331757664",
-    appId: "1:886331757664:web:379e6653055a7bd265df05"
+    apiKey: "API_KEY",
+    authDomain: "AUTH_DOMAIN",
+    projectId: "PROJECT_ID",
+    storageBucket: "STORAGE_BUCKET",
+    messagingSenderId: "SENDER_ID",
+    appId: "APP_ID"
   }
 };
 
